89de2eaace1161b12d4fadb34b745f01c909bbb3
meirfuces <57175607+meirfuces@users.noreply.github.com>
2021-04-21 18:42:16 +0300

    Add files via upload

--- server/main.py
+++ server/main.py
@@ -1,31 +1,66 @@
 from flask import Flask, request
+import filter
+
 from flask_cors import CORS, cross_origin
 import json
 import requests
 app = Flask(__name__)
+import pandas as pd
+import json
+# def CreateData():
+#     data = pd.read_csv("owid-covid-data.csv")
+#     df = pd.DataFrame(data)
+#     df.drop(df.iloc[:, 6:], inplace=True, axis=1)
+#     df = df[df['location'].isin(["Afghanistan", "Israel"])]
+#     # df[df['location'].isin(["Afghanistan"])]   filter
+#
+#     # display
+#     result = df.to_json(orient="table")
+#     parsed = json.loads(result)
+
+    # print(json.dumps(parsed, indent=4))
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
-
+jsonFilenName = 'owid-covid-data.json'
 session = requests.Session()
+
+
+dataCsv = pd.read_csv("owid-covid-data.csv")
+df = pd.DataFrame(dataCsv)
+df.drop(df.iloc[:, 6:], inplace = True, axis = 1)
+
+with open(jsonFilenName) as f:
+    jsonData = json.load(f)
+    
 with open('data.json') as f:
     data1 = json.load(f)
 
 
 from flask import jsonify,make_response
-@app.route('/',methods =['GET'])
+@app.route('/',methods =['GET','POSY-'])
 
 def getTickets():
     print('getStart')
     page = request.args.get('page')
-    print(page)
-    output_dict = [x for x in data1 if "wix " in x['title'] ]
-    output_json = json.dumps(output_dict)
-    return jsonify(data1)
+    # print(page)
+    data = pd.read_csv("owid-covid-data.csv")
+    df = pd.DataFrame(data)
+    # df.drop(df.iloc[:, 6:], inplace=True, axis=1)
+    # df = df[df['location'].isin(["Afghanistan", "Israel"])]
+    # df[df['location'].isin(["Afghanistan"])]   filter
+
+    # display
+    result = df.to_json(orient="table")
+    parsed = json.loads(result)
+    return jsonify(parsed)
+# return JSONP_data
+    # return jsonify(data1)
 
 @app.route('/',methods =['POST'])
-@cross_origin(origin='localhost',headers=['Content- Type','Authorization'])
+@cross_origin(allow_headers=['Content-Type'])
+
 def postTitle():
-    print('post')
+    print('meir ost')
     return jsonify({
         "name": "meir"
     })
@@ -35,8 +70,6 @@ def postTitle():
 @cross_origin(allow_headers=['Content-Type'])
 def search():
     page = session.get(data1, params={'title': 'wix'}).json()
-
-    return jsonify(data1)
-
-
-
+    output_dict = [x for x in data1 if "wix " in x['title']]
+    output_json = json.dumps(output_dict)
+    return jsonify("hello")
