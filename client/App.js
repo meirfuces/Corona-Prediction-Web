41744ade90c2da3897fcb081624a2c6c7a17db2e
meirfuces <57175607+meirfuces@users.noreply.github.com>
2021-04-21 16:13:59 +0300

    Add files via upload

--- client/App.js
+++ client/App.js
@@ -2,78 +2,25 @@ import {Data} from './components/data';
 import React, { Component } from 'react';
 import {data2}  from './components/data2';
 import './App.css';
-import TabkeSick from './components/TableSicks';
 import {createApiClient} from './api';
 const api = createApiClient();
 
-
 class App extends Component {
-  state = {
-    table: [],
-    welcome: "welcome"
-  }
-  async componentDidMount(){
-    console.log("didMount");
-    
-    this.setState({table: await api.getJsonState()})
-    
-   
+  print = async() =>{
+    try {
+      console.log(await api.getJsonState());
+    } catch (error) {
+      console.log(error);
     }
-  sort =() =>{
-    this.setState({welcome: "hello"})
-    // this.setState({...this.state.table.data.sort((a,b)=> a>b) })
-  }
-  print = () =>{
-    const dataF = this.state.table.data.filter(comp => comp.location =="Africa" )
- console.log(dataF);
-this.setState({
-  table: dataF
-})
- console.log(this.state.table.data[0].continent);
-    // try {
-    //   console.log(  api.getJsonState());
-    // } catch (error) {
-    //   console.log(error);
-    // }
-   
-
-  }
-  byCountry = () =>{
-    console.log( api.getJsonByCountry);
 
   }
   render () {
   return (
   
     <div className="stock-container">
-    <p>{this.state.welcome}</p>
+        <button onClick={()=> this.print()}>click</button>
       <h1>corona</h1>
-      <button onClick={()=> this.print()}>click</button>
-      <button onClick={()=> this.sort()}>sort</button>
-      <label for="cars">Choose a country:</label>
-
-<select name="country" id="cars">
-  <option value="israel">israel</option>
-  <option value="tel aviv">tel Aviv</option>
-  <option value="mercedes">USA</option>
-  <option value="audi">Canada</option>
-
-
-</select>
-<ul>
-        { this.state.table.data? this.state.table.data.map(table =>
-        <TabkeSick 
-        key = {table.index}
-        date = {table.date} 
-        country ={table.location}
-        new_cases = {table.new_cases} > fdf </TabkeSick>
-        //  <li>{table.continent}, country {table.location} in data {table.date} new cases {table.new_cases}</li>
-   
-        ): null}
-      </ul>
-{this.state.table.data? 
-this.state.table.data[0].continent : null}
-
+      
      {/* {data2.map((data, key) => {
       
       return (
